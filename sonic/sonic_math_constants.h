5597300adbd64b01245520357c9671fdfca23868
Ned Austin Datiles <ned.austin.datiles@ryerson.ca>
2018-09-09 17:19:49 -0400

    Added the EPSILON constant

--- sonic/sonic_math_constants.h
+++ sonic/sonic_math_constants.h
@@ -4,10 +4,10 @@
 
 namespace sonic {
 	namespace math {
-		const int     MAXINT = (std::numeric_limits<int>::max)();
-		const double  MAXDOUBLE = (std::numeric_limits<double>::max)();
-		const double  MINDOUBLE = (std::numeric_limits<double>::min)();
-		const float   MAXFLOAT = (std::numeric_limits<float>::max)();
-		const float   MINFLOAT = (std::numeric_limits<float>::min)();
+		const int     MAXINT     = (std::numeric_limits<int>::max)();
+		const int     MININT     = (std::numeric_limits<int>::min)();
+		const double  MAXDOUBLE  = (std::numeric_limits<double>::max)();
+		const double  MINDOUBLE  = (std::numeric_limits<double>::min)();
+		const double  EPSILON    = (std::numeric_limits<double>::epsilon)();
 	}
 }
