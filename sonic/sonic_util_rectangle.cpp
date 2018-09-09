68a039e74da50e0c2ff5c08fa72cf732cc6f7ff3
Ned Austin Datiles <ned.austin.datiles@ryerson.ca>
2018-09-08 20:23:11 -0400

    Added the rectangle class

---
+++ sonic/sonic_util_rectangle.cpp
@@ -0,0 +1,229 @@
+#include "sonic_util_rectangle.h"
+
+using sonic::util::rectangle;
+
+sonic::util::rectangle::rectangle(int x, int y, int w, int h) : x(x), y(y), w(w), h(h)
+{
+}
+
+sonic::util::rectangle::operator SDL_Rect()
+{
+	return { x, y, w, h };
+}
+
+sonic::util::rectangle::operator bool()
+{
+	return w != 0 && h != 0;
+}
+
+int sonic::util::rectangle::area() const noexcept
+{
+	return w * h;
+}
+
+rectangle sonic::util::rectangle::move(int offset_x, int offset_y) const noexcept
+{
+	return rectangle(x + offset_x, y + offset_y, w, h);
+}
+
+rectangle sonic::util::rectangle::copy()
+{
+	return rectangle(x, y, w, h);
+}
+
+void sonic::util::rectangle::inflate_ip(int width, int height)
+{
+	w += width;
+	h += height;
+}
+
+rectangle sonic::util::rectangle::inflate(int width, int height) const noexcept
+{
+	return rectangle(x, y, w + width, h + height);
+}
+
+rectangle sonic::util::rectangle::clip(int width, int height) const noexcept
+{
+	return rectangle(x, y, width, height);
+}
+
+void sonic::util::rectangle::union_rect_ip(const rectangle & rect)
+{
+	// Find the top left corner
+	int x_min = x < rect.x ? x : rect.x;
+	int y_min = y < rect.y ? y : rect.y;
+
+	// Find the greatest width
+	int x_max = x + w > rect.x + rect.w ? x + w : rect.x + rect.w;
+
+	// Find the greatest height
+	int y_max = y + h > rect.y + rect.h ? y + h : rect.y + rect.h;
+
+	this->x = x_min;
+	this->y = y_min;
+	this->w = x_max - x_min;
+	this->h = y_max - y_min;
+}
+
+rectangle sonic::util::rectangle::union_rect(const rectangle & rect) const noexcept
+{
+	rectangle r(x, y, w, h);
+	r.union_rect_ip(rect);
+	return r;
+}
+
+bool sonic::util::rectangle::contains_rect(const rectangle & rect) const noexcept
+{
+	return rect.x >= x
+		&&
+		rect.x + rect.w <= x + w
+		&&
+		rect.y >= y
+		&&
+		rect.y + rect.h <= y + h;
+}
+
+bool sonic::util::rectangle::contains_point(const SDL_Point & point) const noexcept
+{
+	return point.x >= x
+		&&
+		point.x <= x + w
+		&&
+		point.y >= y
+		&&
+		point.y <= y + h;
+}
+
+bool sonic::util::rectangle::intersects_with(const rectangle & other) const noexcept
+{
+	return other.x < x + w && other.x + other.w > x && other.y < y + h && other.y + other.h > y ? true : false;
+}
+
+SDL_Point sonic::util::rectangle::center() const noexcept
+{
+	return { x + w / 2, y + h / 2 };
+}
+
+SDL_Point sonic::util::rectangle::top_left() const noexcept
+{
+	return { x, y };
+}
+
+SDL_Point sonic::util::rectangle::top_mid() const noexcept
+{
+	return { x + w / 2, y };
+}
+
+SDL_Point sonic::util::rectangle::top_right() const noexcept
+{
+	return { x + w, y };
+}
+
+SDL_Point sonic::util::rectangle::mid_right() const noexcept
+{
+	return { x + w, y + h / 2 };
+}
+
+SDL_Point sonic::util::rectangle::btm_right() const noexcept
+{
+	return { x + w, y + h };
+}
+
+SDL_Point sonic::util::rectangle::btm_mid() const noexcept
+{
+	return { x + w / 2, y + h };
+}
+
+SDL_Point sonic::util::rectangle::btm_left() const noexcept
+{
+	return { x, y + h };
+}
+
+SDL_Point sonic::util::rectangle::mid_left() const noexcept
+{
+	return { x, y + h / 2 };
+}
+
+int sonic::util::rectangle::width() const noexcept
+{
+	return w;
+}
+
+int sonic::util::rectangle::height() const noexcept
+{
+	return h;
+}
+
+void sonic::util::rectangle::set_width(int val)
+{
+	w = val;
+}
+
+void sonic::util::rectangle::set_height(int val)
+{
+	h = val;
+}
+
+void sonic::util::rectangle::set_center(int x, int y)
+{
+	this->x = x - w / 2;
+	this->y = y - h / 2;
+}
+
+void sonic::util::rectangle::set_top_left(int x, int y)
+{
+	this->x = x;
+	this->y = y;
+}
+
+void sonic::util::rectangle::set_top_mid(int x, int y)
+{
+	this->x = x - w / 2;
+	this->y = y;
+}
+
+void sonic::util::rectangle::set_top_right(int x, int y)
+{
+	this->x = x - w;
+	this->y = y;
+}
+
+void sonic::util::rectangle::set_mid_right(int x, int y)
+{
+	this->x = x - w;
+	this->y = y - h / 2;
+}
+
+void sonic::util::rectangle::set_btm_right(int x, int y)
+{
+	this->x = x - w;
+	this->y = y - h;
+}
+
+void sonic::util::rectangle::set_btm_mid(int x, int y)
+{
+	this->x = x - w / 2;
+	this->y = y - h;
+}
+
+void sonic::util::rectangle::set_btm_left(int x, int y)
+{
+	this->x = x;
+	this->y = y - h;
+}
+
+void sonic::util::rectangle::set_mid_left(int x, int y)
+{
+	this->x = x;
+	this->y = y - h / 2;
+}
+
+bool sonic::util::operator==(const rectangle & lhs, const rectangle & rhs)
+{
+	return lhs.x == rhs.x && lhs.y == rhs.y && lhs.w == rhs.w && lhs.h == rhs.h;
+}
+
+bool sonic::util::operator!=(const rectangle & lhs, const rectangle & rhs)
+{
+	return !(lhs == rhs);
+}
---
+++ sonic/sonic_util_rectangle.h
@@ -0,0 +1,176 @@
+#pragma once
+
+#include <SDL.h>
+
+namespace sonic {
+	namespace util {
+
+		// rectangle class
+		struct rectangle {
+
+			rectangle(int x = 0, int y = 0, int w = 0, int h = 0);
+
+			// Conversion operator to SDL_Rect for functions
+			// taking SDL_Rects
+			operator SDL_Rect();
+
+			// Returns whether or not this rectangle has zero size
+			operator bool();
+
+			// Returns the area of the rectangle
+			int area() const noexcept;
+
+			// Returns a new rectangle moved by the given offset
+			rectangle move(int offset_x, int offset_y) const noexcept;
+
+			// Returns a copy of the calling rectangle
+			rectangle copy();
+
+			// inflates the dimensions of the rectangle in place
+			void inflate_ip(int width, int height);
+
+			// inflates the dimensions of the rectangle
+			rectangle inflate(int width, int height) const noexcept;
+
+			// Returns a clip of the calling rectangle
+			rectangle clip(int width, int height) const noexcept;
+
+			// Combines two rectangles into one in place
+			void union_rect_ip(const rectangle& rect);
+
+			// Combines two rectangles into one
+			rectangle union_rect(const rectangle& rect) const noexcept;
+
+			// Combines many rectangles into one in place
+			template <class T>
+			void union_rect_all_ip(T begin, T end);
+
+			// Combines many rectangles into one
+			template <class T>
+			rectangle union_rect_all(T begin, T end) const noexcept;
+
+			// Checks if a rectangle is inside a rectangle
+			bool contains_rect(const rectangle& rect) const noexcept;
+
+			// Checks if a point is inside a rectangle
+			bool contains_point(const SDL_Point& point) const noexcept;
+
+			// Checks if two rectangles intersect
+			bool intersects_with(const rectangle& other) const noexcept;
+
+			// Checks if multiple rectangles intersect with the caller
+			// The intersecting rectangles are placed inside of result
+			template <class In, class Out>
+			bool intersects_all(In begin, In end, Out intersect_rects) const noexcept;
+
+
+			//------------------------- Getters and Setters --------------------------//
+
+			// Returns the rectangle's width component
+			int width() const noexcept;
+
+			// Returns the rectangle's height component
+			int height() const noexcept;
+
+			// Sets this rectangle's width component
+			void set_width(int val);
+
+			// Sets this rectangle's height component
+			void set_height(int val);
+
+			// Returns the center of the rectangle
+			SDL_Point center() const noexcept;
+
+			// Returns the top left corner of the rectangle
+			SDL_Point top_left() const noexcept;
+
+			// Returns the top mid of the rectangle
+			SDL_Point top_mid() const noexcept;
+
+			// Returns the top right of the rectangle
+			SDL_Point top_right() const noexcept;
+
+			// Returns the mid right of the rectangle
+			SDL_Point mid_right() const noexcept;
+
+			// Returns the btm right of the rectangle
+			SDL_Point btm_right() const noexcept;
+
+			// Returns the btm mid of the rectangle
+			SDL_Point btm_mid() const noexcept;
+
+			// Returns the btm left of the rectangle
+			SDL_Point btm_left() const noexcept;
+
+			// Returns the mid left of the rectangle
+			SDL_Point mid_left() const noexcept;
+
+			// Sets the center of the rectangle
+			void set_center(int x, int y);
+
+			// Sets the top left of the rectangle
+			void set_top_left(int x, int y);
+
+			// Sets the top mid of the rectangle
+			void set_top_mid(int x, int y);
+
+			// /Sets the top right of the rectangle
+			void set_top_right(int x, int y);
+
+			// Sets the mid right of the rectangle
+			void set_mid_right(int x, int y);
+
+			// Sets the btm right of the rectangle
+			void set_btm_right(int x, int y);
+
+			// Sets the mid btm of the rectangle
+			void set_btm_mid(int x, int y);
+
+			// Sets the btm left of the rectangle
+			void set_btm_left(int x, int y);
+
+			// Sets the mid of the rectangle
+			void set_mid_left(int x, int y);
+
+			// Operators
+			friend bool operator==(const rectangle& lhs, const rectangle& rhs);
+			friend bool operator!=(const rectangle& lhs, const rectangle& rhs);
+
+			// rectangle vars
+			int x, y, w, h;
+		};
+
+		template<class T>
+		inline void rectangle::union_rect_all_ip(T begin, T end)
+		{
+			for (T i = begin; i != end; ++i)
+				union_rect_ip(*i);
+		}
+
+		template<class T>
+		inline rectangle rectangle::union_rect_all(T begin, T end) const noexcept
+		{
+			rectangle r(x, y, w, h);
+			r.union_rect_all_ip(begin, end);
+			return r;
+		}
+
+		template<class In, class Out>
+		inline bool rectangle::intersects_all(In begin, In end, Out intersect_rects) const noexcept
+		{
+			bool result = true;
+
+			for (In i = begin; i != end; ++i) {
+				if (!intersects_with(*i)) {
+					result = false;
+				}
+				else {
+					*intersect_rects++ = *i;
+				}
+			}
+
+			return result;
+		}
+
+	}
+}
