e58bb51ea84d39fde00db8fd344aa30ffd9744ad
Ned Austin Datiles <ned.austin.datiles@ryerson.ca>
2018-09-08 20:15:50 -0400

    Added the font resource

---
+++ sonic/sonic_resource_font.cpp
@@ -0,0 +1,58 @@
+#include "sonic_resource_font.h"
+
+sonic::resource::font::font() : m_font(nullptr), filename("")
+{
+}
+
+sonic::resource::font::font(const std::string& path, int font_size)
+	: m_font(std::shared_ptr<TTF_Font>(TTF_OpenFont(path.c_str(), font_size), [](TTF_Font* f) { TTF_CloseFont(f); })), filename(path)
+{
+	if (!m_font)
+		throw std::runtime_error("Could not load font from path: " + path + " -- " + TTF_GetError());
+}
+
+int sonic::resource::font::font_style() const
+{
+	assert(m_font && "Cannot get the style of an invalid font.");
+	return TTF_GetFontStyle(m_font.get());
+}
+
+void sonic::resource::font::set_font_style(int style)
+{
+	assert(m_font && "Cannot set the style of an invalid font.");
+	TTF_SetFontStyle(m_font.get(), style);
+}
+
+int sonic::resource::font::font_outline() const
+{
+	assert(m_font && "Cannot get the outline of an invalid font.");
+	return TTF_GetFontOutline(m_font.get());
+}
+
+void sonic::resource::font::set_font_outline(int outline_size)
+{
+	assert(m_font && "Cannot set the outline of an invalid font.");
+	TTF_SetFontOutline(m_font.get(), outline_size);
+}
+
+int sonic::resource::font::font_height() const
+{
+	assert(m_font && "Cannot get the height of an invalid font.");
+	return TTF_FontHeight(m_font.get());
+}
+
+void sonic::resource::font::resize(int new_size) noexcept
+{
+	assert(filename != "");
+	m_font.reset(TTF_OpenFont(filename.c_str(), new_size), [](TTF_Font* f) { TTF_CloseFont(f); });
+}
+
+sonic::resource::font::operator TTF_Font*()
+{
+	return m_font.get();
+}
+
+sonic::resource::font::operator bool() const noexcept
+{
+	return m_font.get() != nullptr;
+}
---
+++ sonic/sonic_resource_font.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <string>
+#include <memory>
+#include <SDL_ttf.h>
+#include "sonic_resource.h"
+
+namespace sonic {
+	namespace resource {
+
+		// Font class
+		// Used for writing text onto a screen
+		class font final : public sonic::resource::resource_type<font> {
+		public:
+
+			// Constructors
+			font();
+			font(const std::string& path, int font_size);
+
+			// Conversion to a TTF_Font pointer
+			operator TTF_Font*();
+
+			// Checks if this font is initialized correctly
+			operator bool() const noexcept;
+
+			// Returns the this font's style
+			// The returned style is a bitmask composed of the following:
+			// TTF_STYLE_BOLD
+			// TTF_STYLE_ITALIC
+			// TTF_STYLE_UNDERLINE
+			// TTF_STYLE_STRIKETHROUGH
+			// If a style was not set, then TTF_NORMAL_STYLE will be returned
+			int font_style() const;
+
+			// Sets this font's style
+			// the style parameter is a bitmask composed of:
+			// TTF_STYLE_BOLD
+			// TTF_STYLE_ITALIC
+			// TTF_STYLE_UNDERLINE
+			// TTF_STYLE_STRIKETHROUGH
+			void set_font_style(int style);
+
+			// Returns the current size of the font's outline
+			int font_outline() const;
+
+			// Sets the font's outline size
+			// Passing in zero will remove outlines on the font
+			void set_font_outline(int outline_size);
+
+			// Returns the maximum height of all glyphs using this font.
+			// This may be used for rendering text such that they are close 
+			// together vertically as possible. 
+			int font_height() const;
+
+			// Resizes the font
+			void resize(int new_size) noexcept;
+
+		private:
+			std::string filename;
+			std::shared_ptr<TTF_Font> m_font;
+		};
+
+	}
+}
