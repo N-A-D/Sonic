93ebc3ac827579d91b2654d488ad28e36722d26e
Ned Austin Datiles <ned.austin.datiles@ryerson.ca>
2018-09-10 10:57:21 -0400

    added noexcept to member fns

--- sonic/sonic_math_vec2D.cpp
+++ sonic/sonic_math_vec2D.cpp
@@ -13,34 +13,34 @@ sonic::math::vec2D::vec2D(double x, double y) : x(x), y(y)
 {
 }
 
-double sonic::math::vec2D::length() const
+double sonic::math::vec2D::length() const noexcept
 {
 	return sqrt(x * x + y * y);
 }
 
-double sonic::math::vec2D::length_sq() const
+double sonic::math::vec2D::length_sq() const noexcept
 {
 	return pow(x, 2) + pow(y, 2);
 }
 
-double sonic::math::vec2D::dist_to(const vec2D & other) const
+double sonic::math::vec2D::dist_to(const vec2D & other) const noexcept
 {
 	return sqrt(pow(other.x - x, 2) + pow(other.y - y, 2));
 }
 
-double sonic::math::vec2D::dist_to_sq(const vec2D & other) const
+double sonic::math::vec2D::dist_to_sq(const vec2D & other) const noexcept
 {
 	return pow(other.x - x, 2) + pow(other.y - y, 2);
 }
 
-double sonic::math::vec2D::angle_to(const vec2D & other) const
+double sonic::math::vec2D::angle_to(const vec2D & other) const noexcept
 {
 	auto v = this->norm();
 	auto w = other.norm();
 	return rad_to_deg(acos(v.dot(w)));
 }
 
-vec2D sonic::math::vec2D::rotate(double angle) const
+vec2D sonic::math::vec2D::rotate(double angle) const noexcept
 {
 	double rads = deg_to_rad(angle);
 	double t_x = x * cos(rads) - y * sin(rads);
@@ -48,14 +48,14 @@ vec2D sonic::math::vec2D::rotate(double angle) const
 	return vec2D(t_x, t_y);
 }
 
-vec2D sonic::math::vec2D::norm() const
+vec2D sonic::math::vec2D::norm() const noexcept
 {
 	double magnitude = this->length();
 	assert(magnitude != 0 && "Cannot normalize a vector of length zero!");
 	return vec2D(x / magnitude, y / magnitude);
 }
 
-void sonic::math::vec2D::scale(double size)
+void sonic::math::vec2D::scale(double size) noexcept
 {
 	vec2D temp = this->norm();
 	temp.x *= size;
@@ -63,49 +63,49 @@ void sonic::math::vec2D::scale(double size)
 	*this = temp;
 }
 
-double sonic::math::vec2D::dot(const vec2D & other) const
+double sonic::math::vec2D::dot(const vec2D & other) const noexcept
 {
 	return x * other.x + y * other.y;
 }
 
-vec2D sonic::math::vec2D::perp() const
+vec2D sonic::math::vec2D::perp() const noexcept
 {
 	return vec2D(-y, x);
 }
 
-void sonic::math::vec2D::zero()
+void sonic::math::vec2D::zero() noexcept
 {
 	this->x = 0;
 	this->y = 0;
 }
 
-bool sonic::math::vec2D::is_zero() const
+bool sonic::math::vec2D::is_zero() const noexcept
 {
 	return (x * x + y * y) < MINDOUBLE;
 }
 
-vec2D & sonic::math::vec2D::operator+=(const vec2D & other)
+vec2D & sonic::math::vec2D::operator+=(const vec2D & other) noexcept
 {
 	x += other.x;
 	y += other.y;
 	return *this;
 }
 
-vec2D & sonic::math::vec2D::operator-=(const vec2D & other)
+vec2D & sonic::math::vec2D::operator-=(const vec2D & other) noexcept
 {
 	x -= other.x;
 	y -= other.y;
 	return *this;
 }
 
-vec2D & sonic::math::vec2D::operator*=(double scale)
+vec2D & sonic::math::vec2D::operator*=(double scale) noexcept
 {
 	x *= scale;
 	y *= scale;
 	return *this;
 }
 
-vec2D & sonic::math::vec2D::operator/=(double scale)
+vec2D & sonic::math::vec2D::operator/=(double scale) noexcept
 {
 	assert(scale != 0 && "Cannot divide a vector by zero!");
 	x /= scale;
@@ -113,50 +113,50 @@ vec2D & sonic::math::vec2D::operator/=(double scale)
 	return *this;
 }
 
-vec2D sonic::math::operator+(const vec2D & lhs, const vec2D & rhs)
+vec2D sonic::math::operator+(const vec2D & lhs, const vec2D & rhs) noexcept
 {
 	vec2D r = lhs;
 	r += rhs;
 	return r;
 }
 
-vec2D sonic::math::operator-(const vec2D & lhs, const vec2D & rhs)
+vec2D sonic::math::operator-(const vec2D & lhs, const vec2D & rhs) noexcept
 {
 	vec2D r = lhs;
 	r -= rhs;
 	return r;
 }
 
-vec2D sonic::math::operator*(const vec2D & lhs, double scale)
+vec2D sonic::math::operator*(const vec2D & lhs, double scale) noexcept
 {
 	vec2D r = lhs;
 	r *= scale;
 	return r;
 }
 
-vec2D sonic::math::operator*(double scale, const vec2D & rhs)
+vec2D sonic::math::operator*(double scale, const vec2D & rhs) noexcept
 {
 	return rhs * scale;
 }
 
-vec2D sonic::math::operator/(const vec2D & lhs, double scale)
+vec2D sonic::math::operator/(const vec2D & lhs, double scale) noexcept
 {
 	vec2D r = lhs;
 	r /= scale;
 	return r;
 }
 
-vec2D sonic::math::operator/(double scale, const vec2D & rhs)
+vec2D sonic::math::operator/(double scale, const vec2D & rhs) noexcept
 {
-	return rhs * scale;
+	return rhs / scale;
 }
 
-bool sonic::math::operator==(const vec2D & lhs, const vec2D & rhs)
+bool sonic::math::operator==(const vec2D & lhs, const vec2D & rhs) noexcept
 {
 	return approx_equal(lhs.x, rhs.x) && approx_equal(lhs.y, rhs.y);
 }
 
-bool sonic::math::operator!=(const vec2D & lhs, const vec2D & rhs)
+bool sonic::math::operator!=(const vec2D & lhs, const vec2D & rhs) noexcept
 {
 	return !(lhs == rhs);
 }
--- sonic/sonic_math_vec2D.h
+++ sonic/sonic_math_vec2D.h
@@ -11,60 +11,60 @@ namespace sonic {
 			vec2D(double x, double y);
 
 			// Returns the length of the vector
-			double length() const;
+			double length() const noexcept;
 
 			// Returns the length of the vector squared
-			double length_sq() const;
+			double length_sq() const noexcept;
 
 			// Returns the distance to another vector
-			double dist_to(const vec2D& other) const;
+			double dist_to(const vec2D& other) const noexcept;
 
 			// Returns the distance to another vector squared
-			double dist_to_sq(const vec2D& other) const;
+			double dist_to_sq(const vec2D& other) const noexcept;
 
 			// Returns the angle to another vector in degrees
-			double angle_to(const vec2D& other) const;
+			double angle_to(const vec2D& other) const noexcept;
 
 			// Returns a copy of the calling vector rotated by angle
-			vec2D rotate(double angle) const;
+			vec2D rotate(double angle) const noexcept;
 
 			// Returns a copy of the normalized calling vector 
-			vec2D norm() const;
+			vec2D norm() const noexcept;
 
 			// Truncates the length of the vector to size 
-			void scale(double size);
+			void scale(double size) noexcept;
 
 			// Returns the dot product of this with other
-			double dot(const vec2D& other) const;
+			double dot(const vec2D& other) const noexcept;
 
 			// Returns a vector that is perpendicular to the calling vector
-			vec2D perp() const;
+			vec2D perp() const noexcept;
 
 			// Turns the caller into the zero vector
-			void zero();
+			void zero() noexcept;
 
 			// Returns whether or not this vector is the zero vector
-			bool is_zero() const;
+			bool is_zero() const noexcept;
 
 			// Operators
-			vec2D& operator+=(const vec2D& other);
-			vec2D& operator-=(const vec2D& other);
-			vec2D& operator*=(double scale);
-			vec2D& operator/=(double scale);
+			vec2D& operator+=(const vec2D& other) noexcept;
+			vec2D& operator-=(const vec2D& other) noexcept;
+			vec2D& operator*=(double scale) noexcept;
+			vec2D& operator/=(double scale) noexcept;
 
 			double x, y;
 
 		};
 
 		// Additional operators
-		vec2D operator+(const vec2D& lhs, const vec2D& rhs);
-		vec2D operator-(const vec2D& lhs, const vec2D& rhs);
-		vec2D operator*(const vec2D& lhs, double scale);
-		vec2D operator*(double scale, const vec2D& rhs);
-		vec2D operator/(const vec2D& lhs, double scale);
-		vec2D operator/(double scale, const vec2D& rhs);
-		bool operator==(const vec2D& lhs, const vec2D& rhs);
-		bool operator!=(const vec2D& lhs, const vec2D& rhs);
+		vec2D operator+(const vec2D& lhs, const vec2D& rhs) noexcept;
+		vec2D operator-(const vec2D& lhs, const vec2D& rhs) noexcept;
+		vec2D operator*(const vec2D& lhs, double scale) noexcept;
+		vec2D operator*(double scale, const vec2D& rhs) noexcept;
+		vec2D operator/(const vec2D& lhs, double scale) noexcept;
+		vec2D operator/(double scale, const vec2D& rhs) noexcept;
+		bool operator==(const vec2D& lhs, const vec2D& rhs) noexcept;
+		bool operator!=(const vec2D& lhs, const vec2D& rhs) noexcept;
 
 	}
 }
