f06d335dc311683cbd6947ccea6c39b6b94f5d7b
Ned Austin Datiles <ned.austin.datiles@ryerson.ca>
2018-09-14 02:35:16 -0400

    rename the struct vec2D to vec2 and made it a template. vec2D is now a using declaration for syntatic sugar

--- sonic/sonic_math_vec2D.h
+++ sonic/sonic_math_vec2D.h
@@ -1,68 +1,110 @@
 #pragma once
 
+#include <stdexcept>
+#include "sonic_math_functions.h"
+
 namespace sonic {
 	namespace math {
+
 		// 2D space vector
-		struct vec2D {
+		template <class T = double>
+		struct vec2 {
 
 			// Constructors
-			vec2D();
-			vec2D(double x, double y);
+			vec2() = default;
+			vec2(T x, T y) : x(x), y(y) {}
 
 			// Returns the length
-			double length() const noexcept;
+			T length() const noexcept { return sqrt(pow(x, 2) + pow(y, 2)); }
 
 			// Returns the length squared
-			double length_sq() const noexcept;
+			T length_sq() const noexcept { return pow(x, 2) + pow(y, 2); }
 
 			// Returns the distance to other
-			double dist_to(const vec2D& other) const noexcept;
+			T dist_to(const vec2<T>& other) const noexcept { return sqrt(pow(other.x - x, 2) + pow(other.y - y, 2)); }
 
 			// Returns the distance to other squared
-			double dist_to_sq(const vec2D& other) const noexcept;
+			T dist_to_sq(const vec2<T>& other) const noexcept { return pow(other.x - x, 2) + pow(other.y - y, 2); }
 
 			// Returns the angle to another vector in degrees
-			double angle_to(const vec2D& other) const;
+			T angle_to(const vec2<T>& other) const {
+				auto v = this->norm();
+				auto w = other.norm();
+				return sonic::math::rad_to_deg(acos(v.dot(w)));
+			}
 
 			// Returns the dot product between the caller and other
-			double dot(const vec2D& other) const noexcept;
+			T dot(const vec2<T>& other) const noexcept { return x * other.x + y * other.y; }
 
 			// Returns a copy of the caller rotated by angle (degrees)
-			vec2D rotate(double angle) const noexcept;
+			vec2<T> rotate(T angle) const noexcept {
+				auto rads = deg_to_rad(angle);
+				auto t_x = x * cos(rads) - y * sin(rads);
+				auto t_y = x * sin(rads) + y * cos(rads);
+				return vec2<T>(t_x, t_y);
+			}
 
 			// Returns a copy of the caller normalized
-			vec2D norm() const;
+			vec2<T> norm() const {
+				auto mag = this->length();
+				if (mag == 0) throw std::runtime_error("Cannot normalize a vector of length zero!");
+				return vec2<T>(x / mag, y / mag);
+			}
 
 			// Returns a copy of the the caller truncated to size
-			vec2D trunc(double size) const;
+			vec2<T> trunc(T size) const {
+				return this->norm() * size;
+			}
 
 			// Returns a vector that is perpendicular to the calling vector
-			vec2D perp() const noexcept;
+			vec2<T> perp() const noexcept {
+				return vec2<T>(-y, x);
+			}
 
 			// Turns the caller into the zero vector
-			void zero() noexcept;
+			void zero() noexcept { x = 0; y = 0; }
 
 			// Returns whether or not this vector is the zero vector
-			bool is_zero() const noexcept;
+			bool is_zero() const noexcept { return this->length() == 0; }
 
 			// Operators
-			vec2D& operator+=(const vec2D& other) noexcept;
-			vec2D& operator-=(const vec2D& other) noexcept;
-			vec2D& operator*=(double scale) noexcept;
-			vec2D& operator/=(double scale);
+			vec2<T>& operator+=(const vec2<T>& other) noexcept {
+				x += other.x;
+				y += other.y;
+				return *this;
+			}
+			vec2<T>& operator-=(const vec2<T>& other) noexcept {
+				x -= other.x;
+				y -= other.y;
+				return *this;
+			}
+			vec2<T>& operator*=(T scale) noexcept {
+				x *= scale;
+				y *= scale;
+				return *this;
+			}
+			vec2<T>& operator/=(T scale) {
+				if (scale == 0) throw std::runtime_error("Cannot divide a vector by zero!");
+				x /= scale;
+				y /= scale;
+				return *this;
+			}
 
-			double x, y;
+			T x, y;
 
 		};
 
 		// Additional operators
-		vec2D operator+(const vec2D& lhs, const vec2D& rhs) noexcept;
-		vec2D operator-(const vec2D& lhs, const vec2D& rhs) noexcept;
-		vec2D operator*(const vec2D& lhs, double scale) noexcept;
-		vec2D operator*(double scale, const vec2D& rhs) noexcept;
-		vec2D operator/(const vec2D& lhs, double scale);
-		bool operator==(const vec2D& lhs, const vec2D& rhs) noexcept;
-		bool operator!=(const vec2D& lhs, const vec2D& rhs) noexcept;
+		template <class T> vec2<T> operator+(const vec2<T>& lhs, const vec2<T>& rhs) noexcept { return vec2<T>(lhs) += rhs; }
+		template <class T> vec2<T> operator-(const vec2<T>& lhs, const vec2<T>& rhs) noexcept { return vec2<T>(lhs) -= rhs; }
+		template <class T> vec2<T> operator*(const vec2<T>& lhs, double scale) noexcept { return vec2<T>(lhs) *= scale; }
+		template <class T> vec2<T> operator*(double scale, const vec2<T>& rhs) noexcept { return rhs * scale; }
+		template <class T> vec2<T> operator/(const vec2<T>& lhs, double scale) { return vec2<T>(lhs) /= scale; }
+		template <class T> bool operator==(const vec2<T>& lhs, const vec2<T>& rhs) noexcept { return sonic::math::approx_equal(lhs.x, rhs.x) && sonic::math::approx_equal(lhs.y, rhs.y); }
+		template <class T> bool operator!=(const vec2<T>& lhs, const vec2<T>& rhs) noexcept { return !(lhs == rhs); }
 
+		// Syntactic sugar
+		using vec2D = vec2<double>;
+		using vec2Df = vec2<float>;
 	}
 }
